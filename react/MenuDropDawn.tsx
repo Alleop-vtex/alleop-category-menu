2a48bf67160715ab3a2c30e6691c76204f5fd06d
Danylo Iliukha <ijluxa2001@gmail.com>
2021-05-17 11:21:17 +0300

    init commit

---
+++ react/MenuDropDawn.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react'
+
+
+const MenuDropDawn : FunctionComponent = () => {
+    return(
+        <div>
+            "Category Menu DropDawn"
+        </div>
+    )
+}
+export default MenuDropDawn
